f573a7e8c91dc7a1fa96a64689fb72aa00d356b5
Cleison França Amorim <cleison.upgrade@gmail.com>
2022-03-16 22:30:14 -0400

    estudos

--- projeto_perguntas/lib/questionario.dart
+++ projeto_perguntas/lib/questionario.dart
@@ -5,9 +5,13 @@ import 'package:projeto_perguntas/resposta.dart';
 class Questionario extends StatelessWidget {
   final List<Map<String, Object>> perguntas;
   final int perguntaSelecionada;
-  final void Function() responder;
+  final void Function(int) responder;
 
-  Questionario({required this.perguntas, required this.perguntaSelecionada, required this.responder});
+  Questionario({
+    required this.perguntas,
+    required this.perguntaSelecionada,
+    required this.responder,
+  });
 
   bool get temPerguntaSelecionada {
     return perguntaSelecionada < perguntas.length;
@@ -17,9 +21,14 @@ class Questionario extends StatelessWidget {
   Widget build(BuildContext context) {
     List<Map<String, Object>> respostas = temPerguntaSelecionada ? perguntas[perguntaSelecionada].cast()["respostas"] : [];
 
-    return Column(
-      children: [Questao(perguntas[perguntaSelecionada]["texto"].toString()), 
-      ...respostas.map((resp) => Resposta(resp["texto"].toString(), responder)).toList()],
-    );
+    return Column(children: [
+      Questao(perguntas[perguntaSelecionada]["texto"].toString()),
+      ...respostas.map((resp) {
+        return Resposta(
+          resp['texto'].toString(),
+          () => responder(int.parse(resp['pontuacao'].toString())),
+        );
+      }).toList()
+    ]);
   }
 }
--- projeto_perguntas/lib/resultado.dart
+++ projeto_perguntas/lib/resultado.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
 
 class Resultado extends StatelessWidget {
-  const Resultado({Key? key}) : super(key: key);
+  final int pontuacao;
+  final void Function() quandoReiniciarQuestionario;
+  const Resultado(this.pontuacao, this.quandoReiniciarQuestionario);
+
+  String get fraseResultado {
+    if (pontuacao < 8) {
+      return "Parabéns";
+    } else if (pontuacao < 12) {
+      return "Você é bom!";
+    } else if (pontuacao < 16) {
+      return "Impressionante!";
+    } else {
+      return "Nível Jedi!";
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        "Parabéns",
-        style: TextStyle(fontSize: 28),
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Text(
+            fraseResultado,
+            style: TextStyle(fontSize: 28),
+          ),
+        ),
+        TextButton(
+          child: const Text(
+            'Reiniciar?',
+            style: TextStyle(fontSize: 18),
+          ),
+          style: TextButton.styleFrom(
+            primary: Colors.blue,
+          ),
+          onPressed: quandoReiniciarQuestionario,
+        )
+      ],
     );
   }
 }
