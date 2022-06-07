de9d742c3bf5f49b16618a9f13bb6a9485b54438
Cleison França Amorim <>
2022-06-06 20:09:39 -0400

    estudos

--- JamiltonFlutter/app7_youtube/lib/telas/Inicio.dart
+++ JamiltonFlutter/app7_youtube/lib/telas/Inicio.dart
@@ -1,6 +1,8 @@
 import 'package:app7_youtube/Api.dart';
 import 'package:app7_youtube/model/Video.dart';
 import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import "package:flutter_youtube/flutter_youtube.dart";
 
 class Inicio extends StatefulWidget {
   String? pesquisa;
@@ -14,12 +16,42 @@ class Inicio extends StatefulWidget {
 class _InicioState extends State<Inicio> {
   _listarVideos(String pesquisa) {
     Api api = Api();
-    
+
     return api.pesquisar(pesquisa);
   }
 
   @override
+  void initState() {
+    // TODO: implement initState conf iniciais
+    super.initState();
+    print("chamado 1 - initState");
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies para construir a interface precisa de dependencias, ex carregar listagem de videos
+    super.didChangeDependencies();
+    print("chamado 2 - didChangeDependencies");
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose discart de items ex parar um carregamento, pausar algum evento
+    super.dispose();
+    print("chamado 4 - dispose");
+  }
+
+  @override
+  void didUpdateWidget(covariant Inicio oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+    print("chamado 2 - didUpdateWidget");
+  }
+
+  @override
   Widget build(BuildContext context) {
+    //construir a aplicacao
+    print("chamado 3 - build");
     return FutureBuilder<List<Video>?>(
       future: _listarVideos(widget.pesquisa!),
       builder: (context, snapshot) {
@@ -35,22 +67,32 @@ class _InicioState extends State<Inicio> {
                 itemBuilder: (context, index) {
                   List<Video> videos = snapshot.data!;
                   Video video = videos[index];
-                  return Column(
-                    children: [
-                      Container(
-                        height: 200,
-                        decoration: BoxDecoration(
-                            image: DecorationImage(
-                                fit: BoxFit.cover,
-                                image: NetworkImage(
-                                  video.imagem!,
-                                ))),
-                      ),
-                      ListTile(
-                        title: Text(video.titulo!),
-                        subtitle: Text(video.canal!),
-                      )
-                    ],
+                  return GestureDetector(
+                    onTap: () {
+                      FlutterYoutube.playYoutubeVideoById(
+                        apiKey: CHAVE_YOUTUBE_API,
+                        videoId: video.id,
+                        autoPlay: true,
+                        fullScreen: true
+                      );
+                    },
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 200,
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: NetworkImage(
+                                    video.imagem!,
+                                  ))),
+                        ),
+                        ListTile(
+                          title: Text(video.titulo!),
+                          subtitle: Text(video.canal!),
+                        )
+                      ],
+                    ),
                   );
                 },
                 separatorBuilder: (context, index) => const Divider(
